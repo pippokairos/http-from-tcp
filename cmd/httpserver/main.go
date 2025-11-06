bdca607cddbb585428a27e5725eddec188877ece
Filippo <filipporagazzo89@gmail.com>
2025-11-06 18:58:31 +0100

    Add chunked encoding and trailers

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
+	"httpfromtcp/internal/headers"
 	"httpfromtcp/internal/request"
 	"httpfromtcp/internal/response"
 	"httpfromtcp/internal/server"
 )
 
-const port = 42069
+const (
+	port          = 42069
+	httpbinPrefix = "/httpbin/"
+)
+
+func toString(bytes []byte) string {
+	var s string
+	for _, b := range bytes {
+		s += fmt.Sprintf("%02x", b)
+	}
+
+	return s
+}
 
 func respond400() string {
 	return `
@@ -57,14 +74,48 @@ func main() {
 		var body string
 		var status response.StatusCode
 
-		switch req.RequestLine.RequestTarget {
-		case "/yourproblem":
+		endpoint, found := strings.CutPrefix(req.RequestLine.RequestTarget, httpbinPrefix)
+		if found {
+			r, err := http.Get(fmt.Sprintf("https://httpbin.org/%s", endpoint))
+			if err != nil || r.StatusCode != http.StatusOK {
+				log.Println("Error fetching from httpbin")
+				status = response.StatusInternalServerError
+				body = respond500()
+			} else {
+				w.WriteStatusLine(response.StatusOK)
+				w.WriteHeaders(response.GetChunkedHeaders())
+
+				var fullBody []byte
+				for {
+					b := make([]byte, 1024)
+					n, err := r.Body.Read(b)
+					if err != nil {
+						break
+					}
+
+					fullBody = append(fullBody, b[:n]...)
+					w.WriteChunkedBody(b)
+				}
+				w.WriteChunkedBodyDone()
+
+				trailers := headers.NewHeaders()
+				sha := sha256.Sum256(fullBody)
+				trailers["X-Content-SHA256"] = fmt.Sprintf("%x", toString(sha[:]))
+				trailers["X-Content-Length"] = fmt.Sprintf("%d", len(fullBody))
+				err := w.WriteTrailers(trailers)
+				if err != nil {
+					log.Println("Error writing trailers:", err)
+				}
+
+				return
+			}
+		} else if req.RequestLine.RequestTarget == "/yourproblem" {
 			status = response.StatusBadRequest
 			body = respond400()
-		case "/myproblem":
+		} else if req.RequestLine.RequestTarget == "/myproblem" {
 			status = response.StatusInternalServerError
 			body = respond500()
-		default:
+		} else {
 			status = response.StatusOK
 			body = respond200()
 		}
--- internal/response/response.go
+++ internal/response/response.go
@@ -23,6 +23,7 @@ const (
 	StateStatusLine WriterState = "statusLine"
 	StateHeaders    WriterState = "headers"
 	StateBody       WriterState = "body"
+	StateTrailers   WriterState = "trailers"
 	StateDone       WriterState = "done"
 )
 
@@ -104,11 +105,83 @@ func (w *Writer) WriteBody(p []byte) (int, error) {
 	return n, nil
 }
 
+func (w *Writer) WriteChunkedBody(p []byte) (int, error) {
+	if w.State != StateBody {
+		return 0, ErrInvalidState
+	}
+
+	length := string(strconv.FormatInt(int64(len(p)), 16))
+	n, err := w.Writer.Write([]byte(length + "\r\n"))
+	if err != nil {
+		return 0, err
+	}
+
+	m, err := w.Writer.Write(append(p, []byte("\r\n")...))
+	if err != nil {
+		return 0, err
+	}
+
+	return n + m, nil
+}
+
+func (w *Writer) WriteChunkedBodyDone() (int, error) {
+	if w.State != StateBody {
+		return 0, ErrInvalidState
+	}
+
+	n, err := w.Writer.Write([]byte("0\r\n\r\n"))
+	if err != nil {
+		return 0, err
+	}
+
+	w.State = StateTrailers
+
+	return n, nil
+}
+
+func (w *Writer) WriteTrailers(h headers.Headers) error {
+	if w.State != StateTrailers {
+		return ErrInvalidState
+	}
+
+	if len(h) == 0 {
+		w.State = StateDone
+		return nil
+	}
+
+	w.Writer.Write([]byte("0\r\n"))
+
+	for name, value := range h {
+		_, err := fmt.Fprintf(w.Writer, "%s: %s\r\n", name, value)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err := w.Writer.Write([]byte("\r\n"))
+	if err != nil {
+		return err
+	}
+
+	w.State = StateDone
+
+	return nil
+}
+
 func GetDefaultHeaders(contentLength int) headers.Headers {
 	h := headers.NewHeaders()
 	h["Content-Length"] = strconv.Itoa(contentLength)
 	h["Connection"] = "close"
 	h["Content-Type"] = "text/html"
+
+	return h
+}
+
+func GetChunkedHeaders() headers.Headers {
+	h := headers.NewHeaders()
+	h["Transfer-Encoding"] = "chunked"
+	h["Content-Type"] = "text/plain"
+	h["Trailer"] = "X-Content-SHA256, X-Content-Length"
 
 	return h
 }
